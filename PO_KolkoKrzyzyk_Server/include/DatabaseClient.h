6a29f719b692608a503218dd70f8b35a37aa2506
PR3D4T0R15 <mati.m@opoczta.pl>
2024-05-21 15:18:00 +0200

    database class basic connection

--- PO_KolkoKrzyzyk_Server/include/DatabaseClient.h
+++ PO_KolkoKrzyzyk_Server/include/DatabaseClient.h
@@ -1,12 +1,36 @@
 #pragma once
 
 #include <QObject>
+#include <QDebug>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QString>
+
+#include "include/ServerSettings.h"
+
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/instance.hpp>
 
 class DatabaseClient : public QObject
 {
 	Q_OBJECT
 
 public:
-	DatabaseClient(QObject *parent);
+	DatabaseClient(QObject *parent = nullptr);
 	~DatabaseClient();
+
+	void testConn();
+
+
+private:
+	mongocxx::uri generateUrl();
+
+
+
+private:
+	mongocxx::instance* _inst;
+	mongocxx::client* _client;
 };
--- PO_KolkoKrzyzyk_Server/src/DatabaseClient.cpp
+++ PO_KolkoKrzyzyk_Server/src/DatabaseClient.cpp
@@ -1,10 +1,54 @@
 #include "include/DatabaseClient.h"
 
-DatabaseClient::DatabaseClient(QObject *parent)
-	: QObject(parent)
+DatabaseClient::DatabaseClient(QObject *parent): QObject(parent)
 {
+	try
+	{
+		_inst = new mongocxx::instance();
+		_client = new mongocxx::client(generateUrl());
+	}
+	catch (const std::exception& e)
+	{
+		qCritical() << "Exception: " << e.what();
+		exit(-1);
+	}
+	
 }
 
 DatabaseClient::~DatabaseClient()
 {
+	delete _inst;
+}
+
+void DatabaseClient::testConn()
+{
+	try
+	{
+		auto ping_cmd = bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("ping", 1));
+		mongocxx::database db = (*_client)["admin"];
+		bsoncxx::document::view result = db.run_command(ping_cmd.view());
+		qDebug() << "MongoDB CONNECTION....OK";
+	}
+	catch (const std::exception& e)
+	{
+		qCritical() << "Exception: " << e.what();
+		exit(-1);
+	}
+}
+
+mongocxx::uri DatabaseClient::generateUrl()
+{
+	ServerSettings settings;
+	QString url = "mongodb://%1:%2@%3:%4/";
+
+	QString userName = settings.getDbUser();
+	QString userPass = settings.getDbPass();
+	QString ipAddr = settings.getDbHost();
+	QString ipPort = settings.getDbPort();
+
+	url = url.arg(userName).arg(userPass).arg(ipAddr).arg(ipPort);
+
+	settings.deleteLater();
+
+	return mongocxx::uri(url.toStdString());
 }
